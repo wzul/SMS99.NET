8a3b4e0d427df528b185429d09568138143ff728
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2016-07-31 09:24:45 +0800

    Update project.php

--- project.php
+++ project.php
@@ -9,13 +9,12 @@ $billplz_data = array(
 				'Type'	   => '1',
 				'sseqno'   => 'grimace'
 				);
-$process = curl_init("http://login.sms99.net/websmsapi/ISendSMS.aspx");
+$process = curl_init("https://login.sms99.net/websmsapi/ISendSMS.aspx");
 curl_setopt($process, CURLOPT_HEADER, 0);
-//curl_setopt($process, CURLOPT_USERPWD, $api_key . ":");
 curl_setopt($process, CURLOPT_TIMEOUT, 10);
 curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($process, CURLOPT_SSL_VERIFYPEER, FALSE);
 curl_setopt($process, CURLOPT_POSTFIELDS, http_build_query($billplz_data));
 $return = curl_exec($process);
 curl_close($process);
-//$arr = json_decode($return, true);
-var_dump($return);
+echo $return;
